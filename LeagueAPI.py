28d8d0b3b3c66f916e27899c46809d79c887c61a
Kantfan <kantastisch@gmail.comm>
2022-06-23 15:36:04 +0200

    Fixed ignoring first 100 games

--- LeagueAPI.py
+++ LeagueAPI.py
@@ -23,6 +23,15 @@ matchIDs = response.json()
 last_matchID = matchIDs[-1]
 counter = 0
 
+for i, matchID in enumerate(matchIDs):
+        sleep(1.2)
+        match_data = requests.get(f'https://europe.api.riotgames.com/lol/match/v5/matches/{matchID}', headers = headers)
+        match_data = match_data.json()
+        with open(f'matchID{counter}_{i}.json', 'w', encoding='utf-8') as f:
+            json.dump(match_data, f, ensure_ascii=False, indent=4)
+
+counter += 1
+
 while len(matchIDs) == 100:
     sleep(1.2)
     response = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/'+last_matchID, headers = headers)
